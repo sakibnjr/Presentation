a1377024475bdcab9a2e0e4bafb491070536693a
sakibnjr <sakibnjr@proton.me>
2024-11-14 00:52:57 +0600

    Adding Slides

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <a href="https://www.canva.com/design/DAGWLjLxGV4/H7h7XNmg1lN6O5XGp6KHUw/view?utm_content=DAGWLjLxGV4&utm_campaign=designshare&utm_medium=link&utm_source=editor">
+        Start
+      </a>
+    </div>
+  );
+};
+
+export default App;
